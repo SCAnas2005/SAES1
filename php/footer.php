aef4d8639653df95b59c102a1c91d8b15f6cfa89
Lucas93t <147987476+Lucas93t@users.noreply.github.com>
2025-06-25 21:59:07 +0200

    Ajout de documentation

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,6 @@
-<div class="wrapper"></div>
+<div class="wrapper"></div> <!-- Conteneur principal, utilisé pour structurer ou englober le contenu de la page -->
+
 <footer>
+    <!-- Pied de page fixe en bas de la page avec le copyright -->
     <p>&copy; 2025 - Département Informatique, IUT Villetaneuse</p>
 </footer>
